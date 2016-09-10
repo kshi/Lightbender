307deb36b22fd5fc670c12508228c66f711db555
Linda Du <linda.du@columbiaspectator.com>
2016-09-10 03:36:20 -0400

    Added Comments to Data Parsing Script

--- parseDataScript.m
+++ parseDataScript.m
@@ -1,13 +1,17 @@
 %fclose('bundle.out');
 fid = fopen('bundle.out');
-fgetl(fid); % skip first line
+fgetl(fid); % skip first line - header
 line1 = fgetl(fid);
 infoArray = strsplit(line1,' ');
-num_cameras = eval(infoArray{1});
-num_points = eval(infoArray{2}); 
+num_cameras = eval(infoArray{1}); % number of cameras - num of images
+num_points = eval(infoArray{2});  
 
 % loading camera data 
-cameras = zeros(num_cameras*5,3);
+% for each camera entry (format of 'cameras' matrix)
+% <f> <k1> <k2> [focal length, two radial distortion coeffs]
+% <R>           [3x3 matrix representing camera rotation]
+% <t>           [3-vector describing camera traslation]
+cameras = zeros(num_cameras*5,3); 
 for cindex = 0:(num_cameras-1)
     for dindex = 1:5
         arr = strsplit(fgetl(fid),' '); 
@@ -18,8 +22,12 @@ for cindex = 0:(num_cameras-1)
 end
 
 % loading point data
-positions = zeros(3,num_points);
-cloudpts = zeros(num_cameras,num_points*3);
+
+% rows: x,y,z columns: point index
+positions = zeros(3,num_points); 
+% rows: camera index columns: point information (exists-0/1 x y)
+cloudpts = zeros(num_cameras,num_points*3); % info whether point exists in certain views/cameras
+
 for index = 0:(num_points-1)
     %position
     arrp = strsplit(fgetl(fid),' ');
@@ -29,14 +37,15 @@ for index = 0:(num_points-1)
     
     %color (skip)
     fgetl(fid);
+    
     %view list
     arrv = strsplit(fgetl(fid),' ');
     nc = eval(arrv{1});
     for i = 0:(nc-1)
-        cami = eval(arrv{i*4+1+1})+1;
-        cloudpts(cami,index*3+1) = 1; % sets column 1 to true
-        cloudpts(cami,index*3+2) = eval(arrv{i*4+3}); % sets x column
-        cloudpts(cami,index*3+3) = eval(arrv{i*4+4}); % sets y column
+        cam_i = eval(arrv{i*4+1+1})+1; % +1 b/c camera index in .out starts at 0
+        cloudpts(cam_i,index*3+1) = 1; % sets column 1 to true
+        cloudpts(cam_i,index*3+2) = eval(arrv{i*4+3}); % sets x column
+        cloudpts(cam_i,index*3+3) = eval(arrv{i*4+4}); % sets y column
     end
 end
 
